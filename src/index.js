a601a2796e31ba6491915627365ac1e28ff10cc0
imyofather79 <chunsum708@gmail.com>
2021-06-29 21:24:04 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log('%c HI', 'color: firebrick')
+let breeds = [];
+
+function loadImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    fetch(imgUrl)
+    .then (resp => resp.json())
+    .then (data => {
+        data.message.forEach(image => addImage(image))
+    });
+}
+function addImage(dogPicUrl){
+    let container = document.querySelector('#dog-image-container');
+    let newImageEl = document.createElement('img');
+    newImageEl.src = dogPicUrl;
+    container.append(newImageEl);
+}
+
+
+function loadBreedOptions(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+    fetch(breedUrl)
+    .then (resp => resp.json())
+    .then (data => {
+        breeds = Object.keys(data.message);
+        updateBreedList(breeds);
+        addBreedSelectListener();
+    });
+}
+function updateBreedList(breeds){
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+}
+function removeChildren(element){
+    let child = element.lastElementChild;
+    while (child){
+        element.removeChild(child);
+        child = element.lastElementChild;
+    }
+}
+function selectBreedsStartingWith(letter){
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function(e){
+        selectBreedsStartingWith(e.target.value);
+    });
+}
+function addBreed(breed){
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.append(li);
+    li.addEventListener('click', updateColor);
+}
+
+function updateColor(event){
+    e.target.style.color = 'blue';
+}
+
+document.addEventListener('DOMContentLoaded', function (){
+    loadImages();
+    loadBreedOptions();
+});
